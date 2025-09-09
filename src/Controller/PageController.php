79c1e10afcf0957250a09c4efbdb179726d17265
Mauricio J Carrillo <mauricio.jcarrillo@gmail.com>
2025-09-09 00:19:12 -0500

    Add view using twig - templates engine

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,15 +2,20 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class PageController
+class PageController extends AbstractController
 {
     #[Route('/', name: 'home')]
     public function index(): Response
     {
-        return new Response('This is the index page.');
+        // return new Response('This is the index page.');
+
+        return $this->render('pages/home.html.twig', [
+            'title' => 'Welcome to the Home Page',
+        ]);
     }
 }
 
